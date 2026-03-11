fb5ed5cd51058d3eea6c5567b34ef49c39f77c31
wayanwisnu1 <jacobwisnu@gmail.com>
2026-03-11 11:09:35 +0700

    admin bisa chat pelanggan dalam keadaan offline

--- resources/views/admin/conversation.blade.php
+++ resources/views/admin/conversation.blade.php
@@ -494,6 +494,7 @@
                 },
 
                 get canReply() {
+                    if (this.status === 'closed') return true;
                     return this.status === 'active' && this.adminId == this.sessionAdminId;
                 },
 
